e35152e45357bb9352dac16327f8a54b33f84cef
garzaro <garzar@oregonstate.edu>
2017-07-26 13:32:04 -0500

    start worksheet week 5

---
+++ Worksheets_AVL_Heaps/Worksheets_AVL_Heaps/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Worksheets_AVL_Heaps
+//
+//  Created by R G on 7/26/17.
+//  Copyright © 2017 Romano Garza. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
