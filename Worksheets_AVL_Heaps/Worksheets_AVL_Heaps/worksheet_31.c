c450ce593135c7a64f90c73bc344e753bd851187
garzaro <garzar@oregonstate.edu>
2017-07-26 13:36:59 -0500

    create files for worksheet 31

---
+++ Worksheets_AVL_Heaps/Worksheets_AVL_Heaps/worksheet_31.c
@@ -0,0 +1,9 @@
+//
+//  worksheet_31.c
+//  Worksheets_AVL_Heaps
+//
+//  Created by R G on 7/26/17.
+//  Copyright © 2017 Romano Garza. All rights reserved.
+//
+
+#include "worksheet_31.h"
---
+++ Worksheets_AVL_Heaps/Worksheets_AVL_Heaps/worksheet_31.h
@@ -0,0 +1,14 @@
+//
+//  worksheet_31.h
+//  Worksheets_AVL_Heaps
+//
+//  Created by R G on 7/26/17.
+//  Copyright © 2017 Romano Garza. All rights reserved.
+//
+
+#ifndef worksheet_31_h
+#define worksheet_31_h
+
+#include <stdio.h>
+
+#endif /* worksheet_31_h */
