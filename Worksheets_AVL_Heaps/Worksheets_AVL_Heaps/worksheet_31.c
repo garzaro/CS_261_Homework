a440eaac70e6d45a6ab057e8ad3d85bdebec42da
garzaro <garzar@oregonstate.edu>
2017-07-26 19:00:16 -0500

    delete avl practice
    too annoying to complete in text editor, used draw.io instead, and uploaded to google doc

--- Worksheets_AVL_Heaps/Worksheets_AVL_Heaps/worksheet_31.c
+++ Worksheets_AVL_Heaps/Worksheets_AVL_Heaps/worksheet_31.c
@@ -7,3 +7,32 @@
 //
 
 #include "worksheet_31.h"
+
+
+/* 
+
+Rotate Current ("top") Left Single Rotation- Pseudocode
+ 
+ 
+// Set new top node to the current node's right child 
+// Set current nodes new right child to the new top nodes _left_ child
+// Set new top's left child to current node
+// Set height of current node and new top node
+// Return new top node
+ 
+ 
+*/
+
+/*
+ 
+ Rotate Current ("top") Right Single Rotation- Pseudocode
+ 
+ 
+ // Set new top node to the current node's left child
+ // Set current nodes new left child to the new top nodes _right_ child
+ // Set new top's right child to current node
+ // Set height of current node and new top node
+ // Return new top node
+ 
+ 
+ */
