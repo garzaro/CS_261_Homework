e5a29da0af695a9153988ef3146c5c90876b4805
garzaro <garzar@oregonstate.edu>
2017-07-29 09:03:01 -0500

    add ~3/4 functions

--- Worksheets_AVL_Heaps/Worksheets_AVL_Heaps/worksheet_31.c
+++ Worksheets_AVL_Heaps/Worksheets_AVL_Heaps/worksheet_31.c
@@ -8,31 +8,92 @@
 
 #include "worksheet_31.h"
 
+int _h(struct AVLnode * current){
+    if (current == 0) return -1;
+    return current->height;
+}
 
-/* 
 
-Rotate Current ("top") Left Single Rotation- Pseudocode
+void _setHeight (struct AVLnode * current) {
+    int lch = _h(current->left);
+    int rch = _h(current->right);
+    if (lch < rch) current->height = 1 + rch;
+    else current->height = 1 + lch;
+}
+
+
+struct AVLnode * _AVLnodeAdd (struct AVLnode* current, TYPE newValue){
+    struct AVLnode * newnode;
+    if(current == 0){
+        newnode = (struct AVLnode*) malloc(sizeof(struct AVLnode));
+        assert(newnode!=0);
+        newnode->value = newValue;
+        newnode->left =newnode ->right = 0;
+        return newnode;
+    }
+    else if(newValue < current->value){
+        current->left = _AVLnodeAdd(current->left, newValue);
+    }
+    else current->right = _AVLnodeAdd(current->right, newValue);
+    return _balance(current);
+}
+
+
+int _bf(struct AVLnode * current){
+    return _h(current->right) - _h(current->left);
+}
+
+
+struct AVLnode * _balance(struct AVLnode * current){
+    int cbf = _bf(current);
+    if(cbf < -1){
+        if(_bf(current->left) > 0) {
+            current->left = _rotateLeft(current->left);
+        }
+        return _rotateRight(current);
+    }
+    else if (cbf > 1) {
+        if (_bf(current->left) > 0){
+            current->right = _rotateRight(current->right);
+        }
+        return _rotateLeft(current);
+    }
+    _setHeight(current);
+    return current;
+}
+
+
+
+struct AVLnode * _rotateLeft(struct AVLnode * current){
  
- 
-// Set new top node to the current node's right child 
+// Set new top node to the current node's right child
+    struct AVLnode * newTop = current->right;
 // Set current nodes new right child to the new top nodes _left_ child
+    current->right = newTop->left;
 // Set new top's left child to current node
+    newTop->left = current;
 // Set height of current node and new top node
+    _setHeight(current);
+    _setHeight(newTop);
 // Return new top node
- 
- 
-*/
+    return newTop;
+}
 
-/*
+
  
- Rotate Current ("top") Right Single Rotation- Pseudocode
- 
+struct AVLnode * _rotaterRight(struct AVLnode * current){
  
  // Set new top node to the current node's left child
+    struct AVLnode * newTop = current->left; 
  // Set current nodes new left child to the new top nodes _right_ child
+    current->left = newTop->right;
  // Set new top's right child to current node
+    newTop->right = current;
  // Set height of current node and new top node
+    _setHeight(current);
+    _setHeight(newTop);
  // Return new top node
+    return newTop;
+}
  
- 
- */
+
--- Worksheets_AVL_Heaps/Worksheets_AVL_Heaps/worksheet_31.h
+++ Worksheets_AVL_Heaps/Worksheets_AVL_Heaps/worksheet_31.h
@@ -9,6 +9,27 @@
 #ifndef worksheet_31_h
 #define worksheet_31_h
 
+#ifndef TYPE
+#define TYPE  int
+#endif
+
+#include <stdlib.h>
+#include <assert.h>
 #include <stdio.h>
+
+struct AVLnode {
+    TYPE value;
+    struct AVLnode *left;
+    struct AVLnode *right;
+    int height;
+};
+
+int _h(struct AVLnode * current);
+void _setHeight(struct AVLnode * current);
+struct AVLnode * _AVLnodeAdd(struct AVLnode* current, TYPE newValue);
+int _bf (struct AVLnode * current);
+struct AVLnode * _balance(struct AVLnode * current);
+struct AVLnode * _rotateLeft(struct AVLnode * current);
+struct AVLnode * _rotateRight(struct AVLnode * current);
 
 #endif /* worksheet_31_h */
